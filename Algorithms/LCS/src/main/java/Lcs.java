0eb79688fe38e25dbf8a5ae90f3e340934bede02
limdongjin <geniuslim27@gmail.com>
2019-01-26 23:02:43 +0900

    LCS code refactoring

--- Algorithms/LCS/src/main/java/Lcs.java
+++ Algorithms/LCS/src/main/java/Lcs.java
@@ -3,22 +3,21 @@ import java.util.HashMap;
 public class Lcs {
     private HashMap<String, Integer> cache;
 
-    public int length(String x, String y){
-        x = " " + x;
-        y = " " + y;
+    public final int length(String x, String y){
         cache = new HashMap<>();
-        int res = _length(x, y, x.length() - 1, y.length() - 1);
-        return res;
+        return _length(x, y, x.length() - 1, y.length() - 1);
     }
 
-    private int _length(String x, String y, int xi, int yi){
+    private int _length(final String x, final String y, final int xi, final int yi){
         String key_name = xi + "," + yi;
-        int result = 0;
-        if(xi == 0 || yi == 0) return 0;
+        int result;
+        if(xi == -1 || yi == -1) return 0;
         if(cache.containsKey(key_name)) return cache.get(key_name);
 
         if(x.charAt(xi) == y.charAt(yi)) result = _length(x, y, xi -1, yi -1) + 1;
         else result = Math.max(_length(x, y, xi -1, yi), _length(x, y, xi, yi-1));
+
+        cache.put(key_name, result);
 
         return result;
     }
--- Algorithms/LCS/src/test/java/LcsTest.java
+++ Algorithms/LCS/src/test/java/LcsTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LcsTest {
-    Lcs lcs;
+    private Lcs lcs;
     @Before
     public void setUp(){
         lcs = new Lcs();
