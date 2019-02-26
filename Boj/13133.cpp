ad3fda14569ccda711c2d114487e3aa5614fd40c
limdongjin <geniuslim27@gmail.com>
2019-02-26 16:19:03 +0900

    BOJ 13133 testcase add.

--- Boj/13133.cpp
+++ Boj/13133.cpp
@@ -6,13 +6,16 @@ typedef struct Human{
     int status = 1;
 } Human;
 Human *Humans[501];
+
 int initHumans();
-int solve(int n);
+int solve(Human* humans[], int n);
+void TestSolve();
 
 int main(){
     int n;
+    TestSolve();
     n = initHumans();
-    printf("%d", solve(n));
+    printf("%d", solve(Humans, n));
 }
 
 int initHumans(){
@@ -34,17 +37,37 @@ int initHumans(){
     }
     return n;
 }
-
-int solve(int n){
+int solve(Human* humans[], int n){
     int cnt = 0;
     for(int i = 1; i <= n; i++){
-        if(Humans[i]->status == 0 ||
-            Humans[i]->mother == 0 ||
-            Humans[i]->farther == 0) continue;
-        if(Humans[Humans[i]->mother]->status == 0
-           || Humans[Humans[i]->farther]->status == 0)
+        if(humans[i]->status == 0 ||
+           humans[i]->mother == 0 ||
+           humans[i]->farther == 0) continue;
+        if(humans[humans[i]->mother]->status == 0
+           || humans[humans[i]->farther]->status == 0)
             continue;
         cnt++;
     }
     return cnt;
+}
+void TestSolve(){
+    Human *testers[501];
+    testers[1] = new Human{0, 0, 0};
+    testers[2] = new Human{0, 0, 0};
+    testers[3] = new Human{0, 0, 0};
+    testers[4] = new Human{2, 1, 0};
+    testers[5] = new Human{0, 0, 0};
+    testers[6] = new Human{2, 1, 0};
+    testers[7] = new Human{0, 0, 0};
+    testers[8] = new Human{2, 1, 0};
+    testers[9] = new Human{2, 1, 1};
+    testers[10] = new Human{0, 0, 1};
+    testers[11] = new Human{0, 0, 1};
+    testers[12] = new Human{0, 0, 1};
+    testers[13] = new Human{0, 0, 0};
+    testers[14] = new Human{0, 0, 0};
+    testers[15] = new Human{14, 0, 1};
+    testers[16] = new Human{14, 0, 1};
+    testers[17] = new Human{14, 0, 0};
+    assert(solve(testers, 17) == 0);
 }
