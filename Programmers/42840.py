c748b5596b014621962a4b0045a9863676e20e20
limdongjin <geniuslim27@gmail.com>
2019-03-08 16:32:26 +0900

    Programmers 모의고사 refactoring

--- Programmers/42840.py
+++ Programmers/42840.py
@@ -4,52 +4,26 @@
 #
 
 
-def check_answer(person_id, problem_id, answer):
-    checker = {
-        1: {
-            0: 1,
-            1: 2,
-            2: 3,
-            3: 4,
-            4: 5
-        },
-        2: {
-            1: 1,
-            3: 3,
-            5: 4,
-            7: 5
-        },
-        3: {
-            0: 3,
-            1: 3,
-            2: 1,
-            3: 1,
-            4: 2,
-            5: 2,
-            6: 4,
-            7: 4,
-            8: 5,
-            9: 5
-        }
-    }
-    person_ans = 0
-    if person_id == 1:
-        person_ans = checker[person_id][problem_id % 5]
-    elif person_id == 2 and problem_id % 2 == 0:
-        person_ans = 2
-    elif person_id == 2:
-        person_ans = checker[person_id][problem_id % 8]
-    else:
-        person_ans = checker[person_id][problem_id % 10]
-    return int(person_ans == answer)
-
-
 def solution(answers):
-    scores = [sum([check_answer(1, idx, val) for idx, val in enumerate(answers)]),
-              sum([check_answer(2, idx, val) for idx, val in enumerate(answers)]),
-              sum([check_answer(3, idx, val) for idx, val in enumerate(answers)])]
+    scores = [person_score(1, answers), person_score(2, answers), person_score(3, answers)]
     high_persons = [idx + 1 for idx, val in enumerate(scores) if val == max(scores)]
     return high_persons
+
+
+def person_score(person_id, answers):
+    return sum([is_answer(person_id, idx, val) for idx, val in enumerate(answers)])
+
+
+def is_answer(person_id, problem_id, answer):
+    if person_id is 1:
+        person_ans = problem_id % 5 + 1
+    elif person_id is 2 and problem_id % 2 is 0:
+        person_ans = 2
+    elif person_id is 2:
+        person_ans = [1, 3, 4, 5][int((problem_id % 8) / 2)]
+    else:
+        person_ans = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5][problem_id % 10]
+    return int(person_ans == answer)
 
 
 def test_solution():
