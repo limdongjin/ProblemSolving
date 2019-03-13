4dec127f49efddd3d43218cceb566d69134870a6
limdongjin <geniuslim27@gmail.com>
2019-03-13 18:09:50 +0900

    Programmers 자릿수 더하기

--- Programmers/12931.py
+++ Programmers/12931.py
@@ -8,9 +8,19 @@ def solution(n):
     return sum([int(num) for num in str(n)])
 
 
+def solution2(n):
+    return sum(map(int, str(n)))
+
+
 def test_solution():
     assert solution(123) == 6
     assert solution(987) == 24
 
 
+def test_solution2():
+    assert solution(123) == 6
+    assert solution(987) == 24
+
+
 test_solution()
+test_solution2()
