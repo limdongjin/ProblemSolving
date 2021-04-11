09f3644710f2aae68ed40b268f266508a2a60ab3
imdongjin <geniuslim27@gmail.com>
2021-04-11 21:48:29 +0900

    [BOJ] 9467 텀 프로젝트. 오일러 서킷 응용

--- Boj/9466.py
+++ Boj/9466.py
@@ -1,43 +1,47 @@
 import sys
-sys.setrecursionlimit(200000000)
-cnt = 0
+
+sys.setrecursionlimit(1000030)
+input = sys.stdin.readline
+
+
 def main():
     tc = int(input())
     for _ in range(tc):
-        n = int(input())
-        board = [0 for _ in range(n+1)]
-        s = input().split()
-        for i in range(n):
-            board[i+1] = int(s[i])
-        global cnt
-        cnt = 0
-        solve(board)
-        print(n - cnt)
+        input()
+        students = [int(_) - 1 for _ in input().split()]
+
+        print(solve(students))
 
 
-def solve(board):
-    complete = [False for i in range(len(board) + 1)]
-    visited = [False for i in range(len(board) + 1)]
-    for node in range(1, len(board)):
+def solve(students):
+    n_student = len(students)
+    visited = [False for _ in range(n_student)]
+    cnt = 0 # 그룹을
+
+    for node in range(n_student):
         if not visited[node]:
-            dfs(board, visited, complete, node)
+            path = [node]
+            dfs(students, visited, path, node)
+
+            # ex,  path [4, 7, 6, 4]
+            # ex2, path [1, 3, 3]
+            start = next((i for i, x in enumerate(path)
+                          if x == path[-1]
+                          and i != len(path) - 1), None)
+
+            if start is not None:
+                cnt += len(path) - start - 1
+
+    return n_student - cnt
 
 
-def dfs(board, visited, complete, pos):
-    global cnt
-    visited[pos] = True
-    next_node = board[pos]
+def dfs(students, visited, path, node):
+    visited[node] = True
+    next_node = students[node]
+    path.append(next_node)
 
     if not visited[next_node]:
-        dfs(board, visited, complete, next_node)
-    else:
-        if not complete[next_node]:
-            cur = next_node
-            while cur != pos:
-                cnt += 1
-                cur = board[cur]
-            cnt += 1
-    complete[pos] = True
+        dfs(students, visited, path, next_node)
 
 
 main()
