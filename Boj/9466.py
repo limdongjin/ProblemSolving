abe1497da2903470fc3b38f06982965f454b9d28
imdongjin <geniuslim27@gmail.com>
2021-04-11 22:40:27 +0900

    [BOJ] 9466 텀프로젝트. 위상정렬 이용한 풀이 추가

--- Boj/9466.py
+++ Boj/9466.py
@@ -10,9 +10,36 @@ def main():
         input()
         students = [int(_) - 1 for _ in input().split()]
 
-        print(solve(students))
+        print(solve_upgrade(students))
+        # print(solve(students))
 
+# 위상 정렬 아이디어와 유사한듯하다.
+# indegree 가 0 이면 사이클에 속할수 없음을 이용한 풀이
+# 또한 indegree 가 0 인 노드를 그래프에서 제거(가리키는 노드의 indegree 감소) 했을때도
+# 다시 위의 규칙이 적용됨을 이용한것.
+# 2800ms
+def solve_upgrade(students):
+    indegree = [0 for _ in range(len(students))]
+    visited = [False for _ in range(len(students))]
 
+    for st in students:
+        indegree[st] += 1
+
+    cnt = 0
+    for i in range(len(students)):
+        cur = i
+        while indegree[cur] == 0 and not visited[cur]:
+            visited[cur] = True
+            indegree[students[cur]] -= 1
+            cur = students[cur]
+            cnt += 1
+
+    return cnt
+
+# 오일러 서킷에서 아이디어 얻음.
+# dfs 를 돌려서 나온 경로에서 사이클을 찾는 방법
+# 만일 사이클이 있다면 dfs 가 마지막으로 방문한 노드는 반드시 사이클의 시작점이자 끝점일수밖에 없음
+# 5400ms
 def solve(students):
     n_student = len(students)
     visited = [False for _ in range(n_student)]
